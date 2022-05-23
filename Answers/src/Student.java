72755d52225ecf65285bf18f3cd6ef682c8a1add
Mechanikm <mechanik@email.com>
2022-05-23 12:57:37 +0300

    Task Student added

--- Answers/src/Student.java
+++ Answers/src/Student.java
@@ -8,22 +8,50 @@ import java.util.Random;
 
 public class Student {
 
-    String name;
+    String name = "";
     int groupNumber;
     int[] academicPerformance = new int[5];
 
-    public static void main(String[] args) {
-        Student[] students = new Student[10];
+    Student[] students = new Student[10];
+
+    public void makeStudents() {
 
         for (int i = 0; i < students.length; i++) {
-            students[0].name = ("Student" + i);
+
+            students[i] = new Student();
+            students[i].name = "Student" + (i + 1);
 
             Random random = new Random();
 
             students[i].groupNumber = random.nextInt(100) + 1;
 
             for (int j = 0; j < students[i].academicPerformance.length; j++) {
-                students[i].academicPerformance[j] = 5 + random.nextInt(5);
+                students[i].academicPerformance[j] = 8 + random.nextInt(3);
+            }
+        }
+    }
+
+    public void getGoodStudents() {
+
+        for (Student student : students) {
+
+            boolean isGoodStudent = true;
+
+            for (int i = 0; i < 5; i++) {
+                if (student.academicPerformance[i] < 9) {
+                    isGoodStudent = false;
+                }
+            }
+
+            if (isGoodStudent) {
+                System.out.println("Name: " + student.name);
+                System.out.println("Group: " + student.groupNumber);
+                System.out.print("Grades: ");
+
+                for (int i = 0; i < academicPerformance.length; i++) {
+                    System.out.print(student.academicPerformance[i] + " ");
+                }
+                System.out.println();
             }
         }
     }
